a45992e831cb99e8f17f156134af97982d5015ac
Kajetan Mazur <kajetan@webownia.pl>
2017-04-14 09:26:56 +0200

    -in exercise 2 in html file closing script tag -adding exercise 3

---
+++ Exercise - 3 - Animate width and height of a button/Exercise - 3 - Solution/exercise.js
@@ -0,0 +1,14 @@
+$('button#btn1').click(function () {
+    $('#box').animate({
+        width: '500px',
+        height: '500px'
+        },1500);
+ });
+
+$('button#btn2').click(function () {
+    $('#box').animate({
+        width: '150px',
+        height: '150px'
+    },1500);
+
+});
